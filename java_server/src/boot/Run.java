457abc39d8154290c4975a8672df3e3f6a70dce6
Senia Kalma <senia.kalma@gmail.com>
2015-06-21 23:38:29 +0300

    11

--- java_server/src/boot/Run.java
+++ java_server/src/boot/Run.java
@@ -5,6 +5,12 @@ import sModel.Server;
 import sPresenter.SPresenter;
 import sView.serverWindow;
 
+/**
+ * The class runs the Server side of the project.
+ * @author Bar Magnezi(209043827) and Senia Kalma(321969941)
+ * @version 1.0
+ * @since 17.5.2015
+ */
 public class Run {
 	public static void main(String[] args) {
 		MazeClientHandler ch = new MazeClientHandler();
